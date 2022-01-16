5db2e90272b152bc5b9c6a64173565166086d413
Crowdbotics <team@crowdbotics.com>
2022-01-16 13:49:58 +0000

    Initial commit.

---
+++ backend/zooble_32873/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for zooble_32873 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zooble_32873.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "zooble_32873",
+  url: "https://zooble_32873.botics.co",
+  api: "https://zooble_32873.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
